5bf4b5574dc49fbbb89afb7c828dab73744c76f8
Shimul Debnath <debnathshimul909@gmail.com>
2018-08-03 19:57:56 +0600

    Add files via upload

---
+++ app/todo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class todo extends Model
+{
+    public function getBodyAttribute($value)
+    {
+    	return ucfirst($value);
+    }
+
+    public function setBodyattribute($value)
+    {
+    	return $this->attributes['body'] = ucfirst($value);
+    }
+
+    public function getTitleAttribute($value)
+    {
+    	return ucfirst($value);
+    }
+
+    public function setTitleattribute($value)
+    {
+    	return $this->attributes['title'] = ucfirst($value);
+    }
+}
