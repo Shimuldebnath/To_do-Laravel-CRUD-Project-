e3e736fe9e74463541cdc352f01f9c7a99cc3aec
Shimul Debnath <debnathshimul909@gmail.com>
2018-08-10 20:00:29 +0600

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            'newmid' => \App\Http\Middleware\newmid::class,
+           
         ],
 
         'api' => [
@@ -60,5 +60,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'newmid' => \App\Http\Middleware\newmid::class,
     ];
 }
